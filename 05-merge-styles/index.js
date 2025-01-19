eec0abfcbb76cc16c64fb14b6bfb404aac6079e4
StaverLev <leva.staver@mail.ru>
2025-01-20 03:17:08 +0500

    feat: add additional check for file in 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,14 +6,14 @@ const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 const output = fs.createWriteStream(pathToBundle);
 
 function mergeStyles() {
-  fs.readdir(pathToStyles, (err, files) => {
+  fs.readdir(pathToStyles, { withFileTypes: true }, (err, files) => {
     if (err) console.error(err);
 
-    for (const fileName of files) {
-      const pathToFile = path.join(pathToStyles, fileName);
+    for (const file of files) {
+      const pathToFile = path.join(pathToStyles, file.name);
       const fileExt = path.extname(pathToFile);
 
-      if (fileExt === '.css') {
+      if (file.isFile() && fileExt === '.css') {
         const input = fs.createReadStream(pathToFile, 'utf-8');
         input.on('data', (chunk) => output.write(chunk));
       }
